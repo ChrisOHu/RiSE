b727ab788d194a45b5640956a6ced10b0bba08c6
Chris Hu <huhuaishun@gmail.com>
2016-07-12 11:07:10 +0800

    upgrade react & react-native

--- app/events/Events.js
+++ app/events/Events.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import {
+  StyleSheet,
+  Text,
   View,
-  StyleSheet
+  TouchableHighlight
 } from 'react-native'
 
-import {
-  Text
-} from 'app/base'
-
-import { KeyboardAwareScrollView } from 'app/components/KeyboardAwareViews'
+// import { KeyboardAwareScrollView } from 'app/components/KeyboardAwareViews'
+import GiftedListView from '../components/GiftedListView'
 
 class Events extends Component {
   constructor(props) {
@@ -18,13 +17,54 @@ class Events extends Component {
     }
   }
 
+  _onFetch(page = 1, callback, options) {
+    console.debug(page);
+    setTimeout(() => {
+      var rows = ['row '+((page - 1) * 3 + 1), 'row '+((page - 1) * 3 + 2), 'row '+((page - 1) * 3 + 3)];
+      if (page === 3) {
+        callback(rows, {
+          allLoaded: true, // the end of the list is reached
+        });
+      } else {
+        callback(rows);
+      }
+    }, 1000); // simulating network fetching
+  }
+
+  _onPress(rowData) {
+    console.log(rowData + ' pressed');
+  }
+
+  _renderRowView(rowData) {
+    return (
+      <TouchableHighlight
+        style={styles.row}
+        underlayColor='#c8c7cc'
+        onPress={() => this._onPress(rowData)}
+      >
+        <Text>{rowData}</Text>
+      </TouchableHighlight>
+    );
+  }
+
   render() {
     return (
-      <KeyboardAwareScrollView style={[styles.content, this.props.style]}>
-        <View style={styles.card}>
-          <Text>Events</Text>
-        </View>
-      </KeyboardAwareScrollView>
+      <View style={[styles.content, this.props.style]}>
+        <GiftedListView
+          rowView={this._renderRowView}
+          onFetch={this._onFetch}
+          firstLoader={true}
+          pagination={true}
+          refreshable={true}
+          withSections={false}
+          customStyles={{
+            paginationView: {
+              backgroundColor: '#5ae',
+            }
+          }}
+          refreshableTintColor="blue"
+        />
+      </View>
     );
   }
 
@@ -32,7 +72,12 @@ class Events extends Component {
 
 const styles = StyleSheet.create({
   content: {
+    flex: 1,
     backgroundColor: 'green'
+  },
+  row: {
+    padding: 10,
+    height: 44
   }
 });
 
