3005fb5e7cdc9431fa141d2c5d4cf49bcb36caf3
Chris Hu <huhuaishun@gmail.com>
2016-08-03 10:18:51 +0800

    add file uploads to aliyun

--- app/apis/clouds/aliyun/oss/OSS.js
+++ app/apis/clouds/aliyun/oss/OSS.js
@@ -1,28 +1,35 @@
-export BUCKET = 'up-storage';
-export ENDPOINT = 'http://oss-cn-hangzhou.aliyuncs.com';
-export BUCKET_FULL_URL = 'http://up-storage.oss-cn-hangzhou.aliyuncs.com/';
-
 import ALY from 'aliyun-sdk'
 import { OSS_SECRETS } from 'app/secrets'
+import { hmac, Base64 } from 'app/utils/crypto'
+
+const BUCKET = 'up-storage';
+const ENDPOINT = 'http://oss-cn-hangzhou.aliyuncs.com';
+const BUCKET_FULL_URL = 'http://up-storage.oss-cn-hangzhou.aliyuncs.com/';
 
 /**
- * 杭州：http://oss-cn-hangzhou.aliyuncs.com
- * 北京：http://oss-cn-beijing.aliyuncs.com
- * 青岛：http://oss-cn-qingdao.aliyuncs.com
- * 深圳：http://oss-cn-shenzhen.aliyuncs.com
- * 香港：http://oss-cn-hongkong.aliyuncs.com
- * ##### 注意：如果你是在 ECS 上连接 OSS，可以使用内网地址，速度快，没有带宽限制。
- * 杭州：http://oss-cn-hangzhou-internal.aliyuncs.com
- * 北京：http://oss-cn-beijing-internal.aliyuncs.com
- * 青岛：http://oss-cn-qingdao-internal.aliyuncs.com
- * 深圳：http://oss-cn-shenzhen-internal.aliyuncs.com
- * 香港：http://oss-cn-hongkong-internal.aliyuncs.com
+ * see: https://help.aliyun.com/document_detail/31849.html
  */
+const POST_POLICY =
+  "{\"expiration\":\"2048-12-25T12:00:00Z\",\"conditions\":[[\"content-length-range\", 0, 104857600]]}"
+const BASE64_ENCODED_POST_POLICY = Base64.encode(POST_POLICY)
+const SIGNATURE = hmac(OSS_SECRETS.secretAccessKey, BASE64_ENCODED_POST_POLICY, 'base64', 'sha1')
 
-export default new ALY.OSS({
-  accessKeyId: OSS_SECRETS.accessKeyId,
-  secretAccessKey: OSS_SECRETS.secretAccessKey,
-  endpoint: ENDPOINT,
-  // 这是 oss sdk 目前支持最新的 api 版本, 不需要修改
-  apiVersion: '2013-10-15'
-});
+function uploadFile(fileUri, fileKey, fileName, fileType = "application/octet-stream") {
+  let formData = new FormData();
+  formData.append("OSSAccessKeyId", OSS_SECRETS.accessKeyId);
+  formData.append("policy", BASE64_ENCODED_POST_POLICY);
+  formData.append("signature", SIGNATURE);
+  formData.append("key", fileKey);
+  formData.append('file', {uri: fileUri, name: fileName, type: fileType});
+
+  let options = {
+    body: formData,
+    method: 'post'
+  }
+
+  return fetch(BUCKET_FULL_URL, options);
+}
+
+export default {
+  uploadFile
+}
