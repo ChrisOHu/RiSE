d7cd39e8bedb662a212f5e3f75ad43103f516dff
huhuaishun <huhuaishun@icloud.com>
2016-10-31 14:43:57 +0800

    add '--reset-cache' to 'npm start' command to load the .babelrc/.eslintrc changes

--- app/components/homes/HomesMap.js
+++ app/components/homes/HomesMap.js
@@ -40,8 +40,7 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center'
+    justifyContent: 'center'
   }
 });
 
