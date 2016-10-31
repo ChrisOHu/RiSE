e0fd0b5553b867286b2312f634df8025f03e2c7d
huhuaishun <huhuaishun@icloud.com>
2016-10-31 16:31:42 +0800

    update pods

--- app/components/homes/HomesMap.js
+++ app/components/homes/HomesMap.js
@@ -29,7 +29,14 @@ class HomesMap extends Component {
   render() {
     return (
       <NbView style={styles.content} >
-        <MapView />
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />
       </NbView>
     );
   }
